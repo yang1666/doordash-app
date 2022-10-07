6d5b7170d4cd3c12000c81227b9afa4016a8c4a0
yang1666 <yangyimingorelena@gmail.com>
2022-10-07 10:11:08 -0400

    onlineorder frontend I

---
+++ src/utils.js
@@ -0,0 +1,78 @@
+//all api set up
+//login upi
+export const login = (credential) => {
+  //credential ={username:"", password:""}
+  const loginUrl = `/login?username=${credential.username}&password=${credential.password}`;
+
+  return fetch(loginUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error('Fail to log in');
+    }
+  });
+};
+
+export const signup = (data) => {
+  const signupUrl = '/signup';
+
+  return fetch(signupUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error('Fail to sign up');
+    }
+  });
+};
+
+export const getMenus = (restId) => {
+  return fetch(`/restaurant/${restId}/menu`).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error('Fail to get menus');
+    }
+
+    return response.json();
+  });
+};
+
+export const getRestaurants = () => {
+  return fetch('/restaurants').then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error('Fail to get restaurants');
+    }
+
+    return response.json();
+  });
+};
+
+export const getCart = () => {
+  return fetch('/cart').then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error('Fail to get shopping cart data');
+    }
+
+    return response.json();
+  });
+};
+
+export const addItemToCart = (itemId) => {
+  return fetch(`/order/${itemId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error('Fail to add menu item to shopping cart');
+    }
+  });
+};
